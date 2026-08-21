1ff285b6e411887d8ba963959c0b3413e9b99048
Philipp <P.Rodowski@live.de>
2026-08-21 11:25:57 +0200

    Streak-Tracking und Last-Active beim Login implementiert

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,20 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
+
+  def set_streak!
+    today = Date.current
+    if last_active_on == today
+      # bereits heute aktiv, keine Aktion notwendig
+    elsif last_active_on == today - 1
+      self.current_streak += 1
+    else
+      self.current_streak = 1
+    end
+    self.longest_streak = [current_streak, longest_streak].max
+    self.last_active_on = today
+    save
+  end
+
+
 end
